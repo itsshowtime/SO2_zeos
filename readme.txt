3ed6c49ad61eaa71ec2833fc356a7bd74ef600f0
itsshowtime <kilian.pe.co@gmail.com>
2016-11-17 13:48:54 +0100

    quedan cosas

--- readme.txt
+++ readme.txt
@@ -2,4 +2,5 @@ sys_call_table.S	- add a system call identifier to the system call
 sys.c			- syscall implementations
 system.c		- system code 
 entry.S			- handlers
-libc.c			- "link" funcion to system_call_table 
+libc.c			- "link" funcion to system_call_table, ¿user?
+sched.c			- task_switch, scheduler 
--- sys.c
+++ sys.c
@@ -151,7 +151,57 @@ int sys_fork(void)
   return uchild->task.PID;
 }
 
-int sys_clone(void (*function)(void), void *stack){}
+int sys_clone(void (*function)(void), void *stack){
+
+  struct list_head *lhcurrent = NULL;
+  union task_union *uchild;
+
+  /* Any free task_struct? */
+  if (list_empty(&freequeue)) return -ENOMEM;
+
+  lhcurrent=list_first(&freequeue);
+
+  list_del(lhcurrent);
+
+  uchild=(union task_union*)list_head_to_task_struct(lhcurrent);
+
+  /* Copy the parent's task struct to child's */
+  copy_data(current(), uchild, sizeof(union task_union));
+
+  /* new pages dir */
+  allocate_DIR((struct task_struct*)uchild);
+
+/********************cosas por poner***********************/
+
+  uchild->task.PID=++global_PID;
+  uchild->task.state=ST_READY;
+
+  int register_ebp;             /* frame pointer */
+  /* Map Parent's ebp to child's stack */
+  __asm__ __volatile__ (
+    "movl %%ebp, %0\n\t"
+      : "=g" (register_ebp)
+      : );
+  register_ebp=(register_ebp - (int)current()) + (int)(uchild);
+
+  uchild->task.register_esp=register_ebp + sizeof(DWord);
+
+  DWord temp_ebp=*(DWord*)register_ebp;
+  /* Prepare child stack for context switch */
+  uchild->task.register_esp-=sizeof(DWord);
+  *(DWord*)(uchild->task.register_esp)=(DWord)&ret_from_fork;
+  uchild->task.register_esp-=sizeof(DWord);
+  *(DWord*)(uchild->task.register_esp)=temp_ebp;
+
+  /* Set stats to 0 */
+  init_stats(&(uchild->task.p_stats));
+
+  /* Queue child process into readyqueue */
+  uchild->task.state=ST_READY;
+  list_add_tail(&(uchild->task.list), &readyqueue);
+
+  return uchild->task.PID;
+}
 
 #define TAM_BUFFER 512
 
